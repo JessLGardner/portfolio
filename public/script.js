8f5cf41c894285156bac85e22f2c98cb667cf762
Jess Gardner <inanna1130@gmail.com>
2017-09-30 21:23:07 -0400

    fix css on slider

--- public/script.js
+++ public/script.js
@@ -1,9 +1,3 @@
-// $(document).on('click','.navbar-collapse.in',function(e) {
-//   if( $(e.target).is('a') ) {
-//       $(this).collapse('hide');
-//   }
-// });
-
 $(document).ready(function () {
   $(".navbar-nav li a").click(function(event) {
     $(".navbar-collapse").collapse('hide');
